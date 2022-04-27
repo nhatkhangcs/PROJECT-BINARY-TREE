8d1168e24a0188d204f7a406585ce0affcc546e8
bluishfluid666 <nguyenmanhkhang0805@gmail.com>
2022-04-27 23:05:19 +0700

    infixToPostfix updated

--- TESTING_IF_NECCESSARY.cpp
+++ TESTING_IF_NECCESSARY.cpp
@@ -1,6 +1,9 @@
 
 // C++ program to evaluate a prefix expression.
 #include <iostream>
+// #include "./SECTION 1/1a/1a.h"
+// #include "./SECTION 1/1a/1a.h"
+#include "./SECTION 1/validString.h"
 #include <string>
 #include <algorithm>
 #include <vector>
@@ -12,18 +15,27 @@ using namespace std;
 
 int main()
 {
-    map<char,char> dictionary;
-    ifstream dict("dictionary.txt");
-    string s; 
-    int i = 0;
-    while(getline(dict,s)){
-        dictionary.insert(pair<char,char>(s[0],s[2]));
-    }
-    map<char, char>::iterator itr;
-    cout << "\nThe map dictionary is : \n";
-    cout << "\tKEY\tELEMENT\n";
-    for (itr = dictionary.begin(); itr != dictionary.end(); ++itr) {
-        cout << '\t' << itr->first << '\t' << itr->second<< '\n';
-    }
+    // map<char,char> dictionary;
+    // ifstream dict("dictionary.txt");
+    // string s; 
+    // int i = 0;
+    // while(getline(dict,s)){
+    //     dictionary.insert(pair<char,char>(s[0],s[2]));
+    // }
+    // map<char, char>::iterator itr;
+    // cout << "\nThe map dictionary is : \n";
+    // cout << "\tKEY\tELEMENT\n";
+    // for (itr = dictionary.begin(); itr != dictionary.end(); ++itr) {
+    //     cout << '\t' << itr->first << '\t' << itr->second<< '\n';
+    // }
+    // string testStr;// ="(P+(Q*R)/(S-T))";
+    // string testStr="4+)(3*2";
+    string testStr="2//3";
+    // cin>>testStr;
+    cout<<validity(testStr);
+    if(validity(testStr)!=0) errorMessage(testStr);
+    // infixToPrefix(testStr);
+    // cout<<infixToPrefix(testStr);
+    // cout<<infixToPostfix(testStr);
     return 0;
 }
